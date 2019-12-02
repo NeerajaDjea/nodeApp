d022163dce62be54c5f7df05b8c224bd14bd400f
Neeraja Djearaman <neeru.jai@gmail.com>
2019-12-02 09:47:40 +0000

    remove file

--- index.js
+++
@@ -1,8 +0,0 @@
-const profileName = "Eduardo-YGIAH "
-const fetch = require(`node-fetch`);
-
-fetch(`https://api.github.com/users/${profileName}`)
-    .then(response => response.json())
-    .then(profileInfo => {
-        console.log(profileInfo);
-    })
